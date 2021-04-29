a458755a932a5acd7f46316009ae8ab185831609
amichalo421 <andre.michalowski@gmail.com>
2021-04-28 21:06:55 -0400

    Removing links

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 
 export default function AboutPage() {
   return (
@@ -6,7 +5,6 @@ export default function AboutPage() {
       <h1>About</h1>
       <p>This is an app to find the latest local lynn hip-hop and other events.</p>
       <p>Version: 1.0.0 </p>
-      <Link href='/'>Home</Link>
     </div>
   )
 }
