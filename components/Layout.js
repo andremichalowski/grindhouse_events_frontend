6c3945a564753def4991c5864253b8111ed5d467
amichalo421 <andre.michalowski@gmail.com>
2021-04-29 21:08:21 -0400

    Layout style applied

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import styles from '../styles/Layout.module.css'
 export default function Layout({title, keywords, description, children}) {
   return (
     <div>
@@ -7,7 +7,9 @@ export default function Layout({title, keywords, description, children}) {
         <title>{title}</title>
         <meta name='description' content={description} />
       </Head>
-      {children}
+      <div className={styles.container}>
+        {children}
+      </div>
     </div>
   )
 }
