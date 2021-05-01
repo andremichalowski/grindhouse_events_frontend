6072c7393fce978b0ba8307cde06a93ad874fe09
amichalo421 <andre.michalowski@gmail.com>
2021-04-30 21:37:45 -0400

    Create showcase component

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Lynns best hip-hop talent</h1>
+      <h2>Get tickets to the next show</h2>
+      
+    </div>
+  )
+}
