9ef0f832ccdca031cb323e7ba5116251d7bfefcd
amichalo421 <andre.michalowski@gmail.com>
2021-04-30 20:21:11 -0400

    Footer base

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p> Copyright &copy; Grindhouse Events 2021 </p>
+      <p>Footer</p> 
+      <Link href="/about">About Grindhouse</Link>
+    </footer>
+  )
+}
