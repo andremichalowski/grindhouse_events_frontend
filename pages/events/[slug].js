830513f5985f20af3030285445bf6824771edad6
amichalo421 <andre.michalowski@gmail.com>
2021-05-07 21:00:44 -0400

    Removed another '/api/' from fetch url

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -56,7 +56,7 @@ export default function EventPage({evt}) {
 
 export async function getServerSideProps({query: {slug}}) {
   // console.log(slug)
-  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const res = await fetch(`${API_URL}/events/${slug}`)
   const events = await res.json()
 
   return {
