317e73c3b030fe6d91b6d3a30f2bfdc030b9cf0b
amichalo421 <andre.michalowski@gmail.com>
2021-05-17 18:36:16 -0400

    _document.js for modal DOM manipulation

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,9 +1,11 @@
 import moment from 'moment'
+import {FaImage} from 'react-icons/fa'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import {useState} from 'react'
 import {useRouter} from 'next/router'
 import Link from 'next/Link'
+import Image from 'next/image'
 import {API_URL} from '@/config/index'
 import styles from '@/styles/Form.module.css'
 import Layout from '@/components/Layout'
@@ -18,6 +20,7 @@ export default function EditEventPage({evt}) {
     time: evt.time,
     description: evt.description
   })
+  const [imagePreview, setImagePreview] = useState(evt.image ? evt.image.formats.thumbnail.url : null)
 
   const router = useRouter()
 
@@ -133,9 +136,25 @@ export default function EditEventPage({evt}) {
           ></textarea>
         </div>
         
+        
         <input type="submit" value='Update Event' className='btn' />
 
       </form>
+
+      <h2>Event Image</h2>
+      {imagePreview ? (
+        <Image src={imagePreview} height={100} width={170} />
+
+      ) : (<div> 
+            <p>No image uploaded</p>
+          </div>
+      )}
+
+      <div>
+        <button className="btn-secondary">
+          <FaImage /> Set Image
+        </button>
+      </div>
     </Layout>
   )
 }
