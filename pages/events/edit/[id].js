374fe2440a866b07bb5067effe7a888bea6862b9
amichalo421 <andre.michalowski@gmail.com>
2021-05-30 17:40:13 -0400

    Setting the cookie

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -171,9 +171,11 @@ export default function EditEventPage({evt}) {
   )
 }
 
-export async function getServerSideProps({params: {id}}) {
+export async function getServerSideProps({ params: {id}, req }) {
   const res = await fetch(`${API_URL}/events/${id}`)
   const evt = await res.json()
+
+  console.log('cookie from [id].js', req.headers.cookie)
 
   return {
     props: {
