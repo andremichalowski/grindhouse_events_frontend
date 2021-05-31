9a61e17684105f2f00187096e9d9e424968306f4
amichalo421 <andre.michalowski@gmail.com>
2021-05-31 17:32:55 -0400

    event props from auth /events/me

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,6 +1,9 @@
+import {parseCookies} from '@/helpers/index'
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
-export default function DashboardPage() {
+export default function DashboardPage({events}) {
+  console.log(events)
   return (
     <Layout title='User Dashboard'>
       <h1>
@@ -9,4 +12,23 @@ export default function DashboardPage() {
       
     </Layout>
   )
+}
+
+export async function getServerSideProps({req}) {
+  const { token } = parseCookies(req)
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  const events = await res.json()
+
+  return {
+    props: {
+      events
+    },
+  }
 }
