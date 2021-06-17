e076db074f295b0082a0bd4836e9ea8764f6b081
amichalo421 <andre.michalowski@gmail.com>
2021-06-17 17:56:01 -0400

    Updated dynamic edit/delete controls

--- components/DashboardEvent.js
+++ components/DashboardEvent.js
@@ -1,8 +1,12 @@
 import NextLink from 'next/link'
+import {useContext} from 'react'
+import AuthContext from '@/context/AuthContext'
 import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import styles from '@/styles/DashboardEvent.module.css'
 
 export default function DashboardEvent({ evt, handleDelete }) {
+  const { user } = useContext(AuthContext)
+  console.log(user)
   return (
     <div className={styles.event}>
       <h4>
@@ -10,18 +14,24 @@ export default function DashboardEvent({ evt, handleDelete }) {
           <a>{evt.name}</a>
         </NextLink>
       </h4>
-      <NextLink href={`/events/edit/${evt.id}`}>
-        <a className={styles.edit}>
-          <FaPencilAlt /> <span>Edit Event</span>
-        </a>
-      </NextLink>
-      <a
-        href='#'
-        className={styles.delete}
-        onClick={() => handleDelete(evt.id)}
-      >
-        <FaTimes /> <span>Delete</span>
-      </a>
+      { user ? (
+        <div>
+          <NextLink href={`/events/edit/${evt.id}`}>
+            <a className={styles.edit}>
+              <FaPencilAlt /> <span>Edit Event</span>
+            </a>
+          </NextLink>
+          <a
+            href='#'
+            className={styles.delete}
+            onClick={() => handleDelete(evt.id)}
+          >
+            <FaTimes /> <span>Delete</span>
+          </a>
+        </div>
+      ) : ( 
+        <h4 style="text-color: gray"> </h4>
+      )}
     </div>
   )
 }
