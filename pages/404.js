b092183d0673cbf2777e8f6d32e72c4371bb04df
amichalo421 <andre.michalowski@gmail.com>
2021-06-17 12:02:20 -0400

    Temporary removal of triangle icon

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-import FaExlamationTriangle from 'react-icons/fa'
+// import FaExlamationTriangle from 'react-icons/fa'
 import Layout from '@/components/Layout'
 import styles from '@/styles/404.module.css'
 import NextLink from 'next/link'
@@ -8,7 +8,8 @@ export default function NotFoundPage() {
     <Layout title='Page Not Found'>
       <div className={styles.error}>
         <h1>
-          <FaExlamationTriangle />404</h1>
+          {/* <FaExlamationTriangle /> */}
+          404</h1>
         <h4>Sorry there is nothing here.</h4>
         <NextLink href="/">Go Back Home</NextLink>
       </div>
